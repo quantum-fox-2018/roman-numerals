5daa33825d77a409bbbe561e9ba633a9cd5c58b1
Fitrul Islam <fitrul.islam@gmail.com>
2018-03-05 14:47:45 +0700

    done

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,17 @@
-function to_roman (num) {
-  // your implementation code here
+function to_roman (input) {
+  var numRom = [{num:1000,rom:'M'},{num:900,rom:'CM'},{num:500,rom:'D'},{num:400,rom:'CD'},
+                {num:100,rom:'C'},{num:90,rom:'XC'},{num:50,rom:'L'},{num:40,rom:'XL'},
+                {num:10,rom:'X'},{num:9,rom:'IX'},{num:5,rom:'V'},{num:4,rom:'IV'},
+                {num:1,rom:'I'}];
+  var roman = '';
+  for(let i=0;i<numRom.length;i++){
+    if(input >= numRom[i].num){
+      input -= numRom[i].num;
+      roman += numRom[i].rom;
+      i--;
+    }
+  }
+  return roman;
 }
 
 // Drive code
