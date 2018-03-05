930633e5d8977cfb1f416bfafe1c95d8086696fe
agungcaproex <agung.caproex@gmail.com>
2018-03-05 14:19:55 +0700

    second commit day 1

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,13 +1,24 @@
-function to_roman (num) {
-  // your implementation code here
+function toRoman(input) {
+  // start your code here
+  let roman = [['M',1000],['D',500],['CD',400],['C',100],['L',50],['XL',40],['X',10],['IX',9],['V',5],['IV',4],['I',1]]
+  let result = ''
+
+  for(let i=0; i<roman.length; i++){
+    while(input % roman[i][1] < input){
+      result += roman[i][0]
+      input -= roman[i][1]
+    }
+  }
+  return result
 }
 
-// Drive code
 console.log('My totally sweet testing script for new roman\n')
 console.log('input | expected | actual')
-console.log('——————|——————————|———————')
-console.log('4     | IV       | ', to_roman(4))
-console.log('9     | IX       | ', to_roman(9))
-console.log('13    | XIII     | ', to_roman(13))
-console.log('1453  | MCDLIII  | ', to_roman(1453))
-console.log('1646  | MDCXLVI  | ', to_roman(1646))
+console.log('——————|——————————|————————')
+console.log('4     | IV       | ', toRoman(4))
+console.log('9     | IX       | ', toRoman(9))
+console.log('23    | XXIII    | ', toRoman(23))
+console.log('1453  | MCDLIII  | ', toRoman(1453))
+console.log('1646  | MDCXLVI  | ', toRoman(1646))
+
+module.exports = toRoman
