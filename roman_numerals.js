4246c1936f3258139389e3488507abeb89bc660e
marcosumali <marco.sumali90@gmail.com>
2018-03-05 13:43:10 +0700

    add hasil

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,66 @@
 function to_roman (num) {
   // your implementation code here
+  let arrRoman = [];
+    while (num > 0) {
+        if (num <= 3000 && num >= 1000) {
+            let arrRibuan = [];
+            arrRibuan.push('M');
+            num -= 1000;
+            arrRoman.push(arrRibuan.join(''));
+
+        } else if (num < 1000 && num >= 100) {
+            let arrRatusan = [];
+            if (num >= 400 && num < 500) {
+                arrRatusan.push('CD');
+                num -= 400;
+            } else if (num >= 500 && num < 900) {
+                arrRatusan.push('D');
+                num -= 500
+            } else if (num >= 900 && num < 1000) {
+                arrRatusan.push('CM');
+                num -= 900;
+            } else {
+                arrRatusan.push('C');
+                num -= 100;
+            }
+            arrRoman.push(arrRatusan.join(''));
+
+        } else if (num < 100 && num >= 10) {
+            let arrPuluhan = [];
+            if (num >= 40 && num < 50) {
+                arrPuluhan.push('XL');
+                num -= 40;
+            } else if (num >= 50 && num < 90) {
+                arrPuluhan.push('L');
+                num -= 50
+            } else if (num >= 90 && num < 100) {
+                arrPuluhan.push('XC');
+                num -= 90;
+            } else {
+                arrPuluhan.push('X');
+                num -= 10;
+            }
+            arrRoman.push(arrPuluhan.join(''));
+
+        } else if (num < 10) {
+            let arrSatuan = [];
+            if (num == 4) {
+                arrSatuan.push('IV');
+                num -= 4;
+            } else if (num >= 5 && num < 9) {
+                arrSatuan.push('V');
+                num -= 5
+            } else if (num == 9) {
+                arrSatuan.push('IX');
+                num -= 9;
+            } else {
+                arrSatuan.push('I');
+                num -= 1;
+            }
+            arrRoman.push(arrSatuan.join(''));
+        }
+    }
+    return arrRoman.join('');
 }
 
 // Drive code
