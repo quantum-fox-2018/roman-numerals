c85ecbabed92a285cc538dbb250a547f809c1c69
yohanessahrul <yohanessahrul92@gmail.com>
2018-03-05 14:02:01 +0700

    roman-numerals

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,67 +1,32 @@
-/*
-input: numbers 1 - 3000
-process:
-output: strings
-
-*/
-
 function toRoman (num) {
-  // your implementation code here
-  // console.log(String(num)[2]); // 1646 -> 4
-  // console.log(String(num).length); // 1
+  let objRoman = [
+    {angka: 1, romawi:'I'},
+    {angka: 4, romawi:'IV'},
+    {angka: 5, romawi:'V'},
+    {angka: 9, romawi:'IX'},
+    {angka: 10, romawi:'X'},
+    {angka: 40, romawi:'XL'},
+    {angka: 50, romawi:'L'},
+    {angka: 90, romawi:'XC'},
+    {angka: 100, romawi:'C'},
+    {angka: 400, romawi:'CD'},
+    {angka: 500, romawi:'D'},
+    {angka: 900, romawi:'CM'},
+    {angka: 1000, romawi:'M'},
+  ];
   
-  var hasilRoman = '';
-
-  var arrRoman = [
-    { angka: 1,
-      roman: 'I'},
-    { angka: 4,
-      roman: 'IV'},
-    { angka: 5,
-      roman: 'V'},
-    { angka: 9,
-      roman: 'IX'},
-    { angka: 10,
-      roman: 'X'},
-    { angka: 40,
-      roman: 'XL'},
-    { angka: 50,
-      roman: 'L'},
-    { angka: 90,
-      roman: 'XC'},
-    { angka: 100,
-      roman: 'C'},
-    { angka: 400,
-      roman: 'CD'},
-    { angka: 500,
-      roman: 'D'},
-    { angka: 900,
-      roman: 'CM'},
-    { angka: 1000,
-      roman: 'M'}
-  ]
-
-
-while (num != 0) {
-  var angkaMax = arrRoman[0].angka; // 
-  var indexMax = 0;
-  for (var i = 0; i < arrRoman.length; i++) { // 13 // untuk cari angka terdekat
-    if (arrRoman[i].angka <= num && arrRoman[i].angka > angkaMax) {
-      angkaMax = arrRoman[i].angka;
-      indexMax++;
+  let catchRoman = '';
+  
+  for(let i=objRoman.length-1; i>=0; i--){
+    if(num >= objRoman[i].angka){
+      num -= objRoman[i].angka;
+      catchRoman += objRoman[i].romawi;
+      i++;
     }
   }
-
-  // console.log(angkaMax); // 100
-  // console.log(indexMax); // 8
-  // console.log(arrRoman[indexMax].roman); // C
-
-  hasilRoman += arrRoman[indexMax].roman;
-  num -= arrRoman[indexMax].angka;
   
-}
-
-return hasilRoman;
+  return catchRoman;
+    
 
 }
 
@@ -75,4 +40,4 @@ console.log('13    | XIII     | ', toRoman(13))
 console.log('1453  | MCDLIII  | ', toRoman(1453))
 console.log('1646  | MDCXLVI  | ', toRoman(1646))
 
-module.exports = toRoman
+// module.exports = toRoman
