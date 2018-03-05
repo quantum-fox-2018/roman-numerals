7b49627b46f65d1992a06eb5483c96d1e064794e
richoandy <hacktiv8@H8JKT009.local>
2018-03-05 16:52:49 +0700

    challenge done

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,6 +1,104 @@
 function to_roman (num) {
-  // your implementation code here
+  var resultArr = [];
+
+  var temp = "";
+  while(num - 1000 >= 0){
+    temp += "M";
+    num -= 1000;
+  }
+  resultArr.push(temp);
+
+  temp = "";
+  while(num - 900 >= 0){
+    temp += "CM";
+    num -= 900;
+  }
+  resultArr.push(temp);
+
+  temp = "";
+  while(num - 500 >= 0){
+    temp += "D";
+    num -= 500;
+  }
+  resultArr.push(temp);
+
+  temp = "";
+  while(num - 400 >= 0){
+    temp += "CD";
+    num -= 400;
+  }
+  resultArr.push(temp);
+
+  temp = "";
+  while(num - 100 >= 0){
+    temp += "C";
+    num -= 100;
+  }
+  resultArr.push(temp);
+
+  temp = "";
+  while( num - 90 >= 0){
+    temp += "XC"
+    num -= 90;
+  }
+  resultArr.push(temp);
+
+  temp = "";
+  while(num - 50 >= 0){
+    temp += "L";
+    num -= 50;
+  }
+  resultArr.push(temp);
+
+  temp = "";
+  while(num - 40 >= 0){
+    temp += "XL";
+    num -= 40;
+  }
+  resultArr.push(temp);
+
+  temp = "";
+  while(num - 10 >= 0){
+    temp += "X";
+    num -= 10;
+  }
+  resultArr.push(temp);
+
+  temp = "";
+  while(num - 9 >= 0){
+    temp += "IX";
+    num -= 9;
+  }
+  resultArr.push(temp);
+
+  var temp = "";
+  while(num - 5 >= 0){
+    temp += "V";
+
+    num -= 5;
+  }
+  resultArr.push(temp);
+
+  var temp = "";
+  while(num - 4 >= 0){
+    temp += "IV";
+    num -= 4;
+  }
+  resultArr.push(temp)
+
+  var temp = "";
+  while(num - 1 >= 0){
+    temp += "I";
+    num -= 1;
+  }
+  resultArr.push(temp);
+
+  var result =  resultArr.join("");
+  return result;
 }
+
+
+
 
 // Drive code
 console.log('My totally sweet testing script for new roman\n')
