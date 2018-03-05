94ee16fea2a6773d7625d050517e2b53bf67cc76
Natascha Lamsu <tazcha15@gmail.com>
2018-03-05 19:10:57 +0700

    roman numerals clear

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,17 @@
 function to_roman (num) {
   // your implementation code here
+  var arrNum = [ [1, "I"], [4, "IV"], [5, "V"], [9, "IX"], [10, "X"], [40, "XL"], [50, "L"], [90, "XC"],
+    [100, "C"], [400, "CD"], [500, "D"], [900, "CM"], [1000, "M"] ];
+  var str = "";
+  while(num > 0) {
+    for(var n = arrNum.length-1; n >=0 ;n--) {
+      if(num >= arrNum[n][0]) {
+        str += arrNum[n][1];
+        num -= arrNum[n][0];
+      }
+    }
+  }
+  return str;
 }
 
 // Drive code
