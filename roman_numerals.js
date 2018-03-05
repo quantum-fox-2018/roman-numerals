d916f314075b38fb7f73d549aeb0031687f29592
fastabiqul1993 <fastabiqul1993@gmail.com>
2018-03-05 22:28:53 +0700

    AdhyFastabiqul

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,20 @@
 function to_roman (num) {
   // your implementation code here
+  var result = ''
+  var latin = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+  var roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
+
+  for (var i = 0; i < latin.length; i++) {
+    
+    while (num%latin[i] < num) {
+
+      result += roman[i]
+
+      num -= latin[i]
+    }
+  }
+
+  return result
 }
 
 // Drive code
