70ab831feca13dccb68da5c8ce025ea4f44e0914
Dani Damara <danny12march@gmail.com>
2018-03-05 17:36:19 +0700

    Mengganti semua function menjadi while

--- ex3-w1.js
+++ ex3-w1.js
@@ -1,168 +1,75 @@
 //Roman Numerals
-function under10(num, roman){
-
-  if(num < 5){
-      if(num === 4){
-          roman += 'IV';
-      }else{
-          for(let i = 1; i <= num ;i++){
-              roman += 'I';
-          }
-      }
-  }else if (num < 10) {
-    if(num === 5){
-        roman += 'V';
-    }else if (num === 9) {
-        roman += 'IX';
-    }else{
-        roman += 'V';
-        for(let i = 6; i <= num ;i++){
-            roman += 'I';
-        }
-    }
-  }
-
-  return roman;
-}
-
-function under50(num, roman){
-    if(num < 50){
-        if(num === 10){
-            roman += 'X';
-        }else if (num >= 40 && num <49) {
-            if(num === 40){
-                roman += 'XL';
-
-            }else{
-                roman += 'XL';
-                return under10(num-40, roman);
-            }
-        }else if(num === 49) {
-            roman += 'XLIX';
-        }else {
-            while(num >= 10){
-                num -= 10;
-                roman += 'X';
-            }
-            if(num !== 0){
-                return under10(num, roman);
-            }
-        }
-    }
-    return roman;
-
-}
-
-function under100(num, roman){
-    if(num < 100){
-        if(num === 50){
-            roman = 'L';
-        }else if (num >= 90 && num <= 99) {
-            if(num === 90){
-                roman += 'XC';
-
-            }else{
-                roman += 'XC';
-                return under50(num-90, roman);
-            }
-        }else {
-            while(num >= 50){
-                num -= 50;
-                roman += 'L';
-            }
-            if(num !== 0){
-                return under50(num, roman);
-            }
-        }
-    }
-    return roman;
-
-}
-
-function under500(num, roman){
-  if(num < 500){
-      if(num === 100){
-          roman = 'C';
-      }else if (num >= 400 && num <= 499) {
-          if(num === 400){
-              roman += 'CD';
-
-          }else{
-              roman += 'CD';
-              return under100(num-400, roman);
-          }
-      }else {
-          while(num >= 100){
-              num -= 100;
-              roman += 'C';
-          }
-          if(num !== 0){
-              return under100(num, roman);
-          }
-      }
-  }
-  return roman;
-}
-
-function under1000(num, roman){
-  if(num < 1000){
-      if(num === 500){
-          roman = 'D';
-      }else if (num >= 900 && num <= 999) {
-          if(num === 900){
-              roman += 'CM';
-
-          }else{
-              roman += 'CM';
-              return under500(num-900, roman);
-          }
-      }else {
-          while(num >= 500){
-              num -= 500;
-              roman += 'D';
-          }
-          if(num !== 0){
-              return under500(num, roman);
-          }
-      }
-  }
-  return roman;
-}
-
-function above1000(num, roman){
-  if(num >= 1000){
-      if(num === 1000){
-          roman = 'M';
-      }else {
-          while(num >= 1000){
-              num -= 1000;
-              roman += 'M';
-          }
-          if(num !== 0){
-              return under1000(num, roman);
-          }
-      }
-  }
-  return roman;
-}
 
 function toRoman(num){
-    let roman = '';
-    if(num < 10){
-        return under10(num, roman);
-    }else if (num < 50) {
-        return under50(num, roman);
-    }else if (num < 100) {
-        return under100(num, roman);
-    }else if (num < 500) {
-        return under500(num, roman);
-    }else if(num < 1000) {
-        return under1000(num, roman);
-    }else {
-        return above1000(num, roman);
-    }
+  var hasil = '';
 
-    return roman;
+  while(num >= 1000 ){
+    hasil += 'M';
+    num -= 1000;
+  }
+
+  if(num === 900){
+    hasil += 'CM';
+    num -= 900;
+  }else {
+    while(num >= 500){
+      hasil += 'D';
+      num -= 500;
+    }
+  }
+
+  if(num >= 400){
+    hasil += 'CD';
+    num -= 400;
+  }else {
+    while(num >= 100){
+      hasil += 'C';
+      num -= 100;
+    }
+  }
+
+  if(num >= 90){
+    hasil += 'XC';
+    num -= 90;
+  }else{
+    while(num >= 50){
+      hasil += 'L';
+      num -= 50;
+    }
+  }
+
+
+  if(num >= 40){
+    hasil += 'XL';
+    num -= 40;
+  }else{
+    while(num >= 10){
+      hasil += 'X';
+      num -= 10;
+    }
+  }
+
+  if(num === 9){
+    hasil += 'IX';
+    num -= 9;
+  }else{
+    while (num >= 5) {
+      hasil += 'V';
+      num -= 5;
+    }
+  }
+
+
+  if(num === 4){
+    hasil += 'IV';
+  }else{
+    while(num >= 1){
+      hasil += 'I';
+      num -= 1;
+    }
+  }
+
+  return hasil;
 }
 
 console.log('My totally sweet testing script\n');
