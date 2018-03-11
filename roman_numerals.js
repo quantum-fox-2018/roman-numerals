c40a57c568b9f907eb50dce5dfd4351db24fece1
asepridwan1989 <aseprmail@gmail.com>
2018-03-12 03:30:39 +0700

    upload

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,16 @@
 function to_roman (num) {
   // your implementation code here
+  var roman = [[1000, 'M'],[900, 'CM'],[500, 'D'],[400, 'CD'],[100, 'C'],[90, 'XC'],
+  [50, 'L'],[40, 'XL'],[10, 'X'],[9, 'IX'],[5, 'V'],[4, 'IV'],[1, 'I']]
+  var result = '';
+  for (var i = 0; i < roman.length; i++) {
+
+    while (num >= roman[i][0] ) {
+      num -= roman[i][0];
+      result += roman[i][1];
+    }
+  }
+  return result;
 }
 
 // Drive code
